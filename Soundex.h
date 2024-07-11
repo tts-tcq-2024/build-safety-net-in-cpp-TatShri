e3d5dba90c80af60d80d7f7e0e4e309592900f22
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-11 12:02:17 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,16 +1,10 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include <unordered_map>
-#include <cctype>
 #include <string>
-#include <numeric>
+#include <vector>
 
-char getSoundexCode(char c);
-bool isHW(char c);
-bool shouldAppend(char code, char prevCode, char currentChar);
-std::string accumulateSoundexCodes(const std::string& name);
-std::string padSoundex(const std::string& soundex);
-std::string generateSoundex(const std::string& name);
+// Public function for generating Soundex code
+std::string generateSoundex(const std::string& name, const std::vector<std::pair<char, char>>& soundexMap);
 
 #endif // SOUNDEX_H
