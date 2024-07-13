b2417ea4b2eaed8a328095d62ecc3013736294e2
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-13 23:49:37 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -2,20 +2,7 @@
 #define SOUNDEX_H
 
 #include <string>
-#include <vector>
 
-class Soundex {
-public:
-    Soundex();
-
-    std::string generateSoundex(const std::string& name) const;
-
-private:
-    std::unordered_map<char, char> soundexMap;
-
-    char mapCharToSoundex(char c) const;
-    bool isSeparator(char currentChar) const;
-    void appendValidSoundexCode(std::string& soundex, char code, char& prevCode, char currentChar) const;
-};
+std::string generateSoundex(const std::string& name);
 
 #endif // SOUNDEX_H
