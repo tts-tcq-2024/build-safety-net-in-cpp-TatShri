02b7dcfa2136ebcc751fd40e15afc8fc991370ac
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-14 12:58:44 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,30 +1,48 @@
 #include "Soundex.h"
-#include <gtest/gtest.h>
+#include <gtest/gtest.h>    
 
-TEST(GenerateSoundexTest, BasicFunctionality) {
+TEST_F(GenerateSoundexTest, BasicFunctionality) {
+    EXPECT_EQ(generateSoundex("Saitama"), "S350");
     EXPECT_EQ(generateSoundex("Naruto"), "N630");
-    EXPECT_EQ(generateSoundex("Sasuke"), "S000");
-    EXPECT_EQ(generateSoundex("Luffy"), "L100");
+    EXPECT_EQ(generateSoundex("Lelouch"), "L200");
 }
 
-TEST(GenerateSoundexTest, EmptyName) {
+TEST_F(GenerateSoundexTest, EmptyString) {
     EXPECT_EQ(generateSoundex(""), "0000");
 }
 
-TEST(GenerateSoundexTest, NonAlphabeticCharacters) {
-    EXPECT_EQ(generateSoundex("Goku123"), "G200");
-    EXPECT_EQ(generateSoundex("Kakarot@"), "K263");
-    EXPECT_EQ(generateSoundex("!Vegeta"), "V230");
+TEST_F(GenerateSoundexTest, NonAlphabeticCharacters) {
+    EXPECT_EQ(generateSoundex("Goku123"), "G000");
+    EXPECT_EQ(generateSoundex("Ryuk@DeathNote"), "R235");
 }
 
-TEST(GenerateSoundexTest, ShortNames) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("Ai"), "A000");
-    EXPECT_EQ(generateSoundex("Yu"), "Y000");
+TEST_F(GenerateSoundexTest, MixedCase) {
+    EXPECT_EQ(generateSoundex("kAGOME"), "K500");
+    EXPECT_EQ(generateSoundex("eRen yEager"), "E652");
 }
 
-TEST(GenerateSoundexTest, LongNames) {
-    EXPECT_EQ(generateSoundex("Monkey D. Luffy"), "M523");
-    EXPECT_EQ(generateSoundex("Kurosaki Ichigo"), "K622");
-    EXPECT_EQ(generateSoundex("Sakura Haruno"), "S626");
+TEST_F(GenerateSoundexTest, AllVowels) {
+    EXPECT_EQ(generateSoundex("Aiueo"), "A000");
+}
+
+TEST_F(GenerateSoundexTest, SingleCharacterName) {
+    EXPECT_EQ(generateSoundex("Z"), "Z000");
+}
+
+TEST_F(GenerateSoundexTest, ShortNames) {
+    EXPECT_EQ(generateSoundex("Ichigo"), "I200");
+    EXPECT_EQ(generateSoundex("Mio"), "M000");
+}
+
+TEST_F(GenerateSoundexTest, LongName) {
+    EXPECT_EQ(generateSoundex("Kotobukiya"), "K312");
+}
+
+TEST_F(GenerateSoundexTest, NameWithHOrW) {
+    EXPECT_EQ(generateSoundex("Mhnmi"), "M200");
+    EXPECT_EQ(generateSoundex("Haruhi"), "H600");
+}
+
+TEST_F(GenerateSoundexTest, NameWithApostrophe) {
+    EXPECT_EQ(generateSoundex("Itachi'Uchiha"), "I320");
 }
